0b476c908992599e396a88759200696d81a150b0
nigel-riss <mr.kurenkov@gmail.com>
2018-02-06 14:27:25 +0200

    Check gulp workflow (return???) Fix gulp workflow (semicolons, indent)

--- gulp/tasks/styles.js
+++ gulp/tasks/styles.js
@@ -6,15 +6,15 @@ const autoprefixer = require("gulp-autoprefixer");
 
 gulp.task("styles", () => {
     return gulp.src("./src/scss/**/*.scss")
-    .pipe(sass({importer: SassModuleImporter()}))
-    .on("error", notify.onError(function(error) {
-        return {
-            ttile: "Styles",
-            message: error.message
-        };
-    }))
-    .pipe(autoprefixer({
-        browsers: "last 2 versions"
-    }))
-    .pipe(gulp.dest("./dist"));
-})
+        .pipe(sass({importer: SassModuleImporter()}))
+        .on("error", notify.onError(function(error) {
+            return {
+                ttile: "Styles",
+                message: error.message
+            };
+        }))
+        .pipe(autoprefixer({
+            browsers: "last 2 versions"
+        }))
+        .pipe(gulp.dest("./dist"));
+});
