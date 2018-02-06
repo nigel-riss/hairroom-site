75af8a1263e7c36de09daba902c684879647c7ee
Verdan777 <vereshhagin98@mail.ru>
2018-02-06 13:22:27 +0200

    superficially markup and styling

--- gulp/tasks/pages.js
+++
@@ -1,7 +0,0 @@
-const gulp = require("gulp");
-const pages = require("gulp-gh-pages");
-
-gulp.task('pages', () => {
-    return gulp.src('./dist/**/*')
-    .pipe(pages())
-});
--- gulpfile.js
+++ gulpfile.js
@@ -2,4 +2,3 @@ require("./gulp/tasks/pug.js");
 require("./gulp/tasks/styles.js");
 require("./gulp/tasks/scripts.js");
 require("./gulp/tasks/watch.js");
-require("./gulp/tasks/pages.js");
