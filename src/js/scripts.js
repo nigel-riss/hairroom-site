a486b81cf74562bada28f51f63c00b867fe1e9ab
nigel-riss <mr.kurenkov@gmail.com>
2018-02-07 16:10:07 +0200

    Fix tabs in about section for mobile Small fixes style and vars

--- src/js/scripts.js
+++ src/js/scripts.js
@@ -1,10 +1,10 @@
 import MobileMenu from "./modules/MobileMenu";
-import Tabs from './modules/Tabs';
+// import Tabs from './modules/Tabs';
 import ScrollSection from './modules/ScrollSection';
 import ScrollSpy from './modules/ScrollSpy';
 
 
 const mobilemenu = new MobileMenu();
-const tabs = new Tabs();
+// const tabs = new Tabs();
 const scrollSpy = new ScrollSpy();
 const scrollSection = new ScrollSection();
